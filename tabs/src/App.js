eb3339236361117c06801420eed1d37f8d8e7aed
Charanaraj N <goudacharanraj760@gmail.com>
2023-05-22 16:39:41 +0530

    refactor: optimized

--- tabs/src/App.js
+++ tabs/src/App.js
@@ -1,33 +1,28 @@
 import "./App.css";
+import { useState } from "react";
+import { data } from "./data";
 
 function App() {
+  const [activeTab, setActiveTab] = useState("HTML");
   return (
     <div className="App">
       <h1>Tabs</h1>
       <div>
-        <button>HTML</button>
-        <button>CSS</button>
-        <button>JavaScript</button>
+        {data.map((tab) => (
+          <button
+            className={activeTab === tab.label ? "active" : undefined}
+            onClick={() => setActiveTab(tab.label)}
+            key={tab.id}
+          >
+            {tab.label}
+          </button>
+        ))}
       </div>
-      <div>
-        <p>
-          HyperText Markup Language (HTML) is the basic scripting language used
-          by web browsers to render pages on the world wide web. HyperText
-          allows a user to click a link and be redirected to a new page
-          referenced by that link.
-        </p>
-        <p>
-          Cascading Style Sheets (CSS) is a stylesheet language used to describe
-          the presentation of a document written in HTML or XML (including XML
-          dialects such as SVG, MathML or XHTML). CSS describes how elements
-          should be rendered on screen, on paper, in speech, or on other media.
-        </p>
-        <p>
-          JavaScript is a cross-platform, object-oriented scripting language
-          used to make webpages interactive (e.g., having complex animations,
-          clickable buttons, popup menus, etc.). There are also more advanced
-          server side versions of JavaScript such as Node.
-        </p>
+      <div className="tab-description">
+        {data.map(
+          (tabDesc) =>
+            tabDesc.label === activeTab && <p>{tabDesc.description}</p>
+        )}
       </div>
     </div>
   );
---
+++ tabs/src/data.js
@@ -0,0 +1,27 @@
+export const data = [
+  {
+    id: 1,
+    label: "HTML",
+    description: `HyperText Markup Language (HTML) is the basic scripting language
+        used by web browsers to render pages on the world wide web.
+        HyperText allows a user to click a link and be redirected to a new
+        page referenced by that link.`,
+  },
+  {
+    id: 2,
+    label: "CSS",
+    description: `Cascading Style Sheets (CSS) is a stylesheet language used to
+        describe the presentation of a document written in HTML or XML
+        (including XML dialects such as SVG, MathML or XHTML). CSS describes
+        how elements should be rendered on screen, on paper, in speech, or
+        on other media.`,
+  },
+  {
+    id: 3,
+    label: "JavaScript",
+    description: `JavaScript is a cross-platform, object-oriented scripting language
+        used to make webpages interactive (e.g., having complex animations,
+        clickable buttons, popup menus, etc.). There are also more advanced
+        server side versions of JavaScript such as Node.`,
+  },
+];
